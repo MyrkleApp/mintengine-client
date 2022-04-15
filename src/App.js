ace74a742f6edd8b475d9e35948432d16bc7b2e5
Sammy <sammyoziegbe@gmail.com>
2022-04-15 14:21:07 +0100

    lazy load pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import React, { Suspense } from 'react'
 import './App.css';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import Header from './components/Header/Header';
 import Auth from './pages/Auth/Auth';
-import LandingPage from './pages/LandingPage/LandingPage';
+// import LandingPage from './pages/LandingPage/LandingPage';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ScrollToTop from './Hooks/ScrollToTop';
 import WalletSetup from './pages/WalletSetup/WalletSetup';
 import CreateNewWallet from './pages/CreateNewWallet/CreateNewWallet';
 import VerifyAlgoWallet from './pages/VerifyAlgoWallet/VerifyAlgoWallet';
-import VerifyRippleWallet from './pages/VerifyRippleWallet/VerifyRippleWallet';
+// import VerifyRippleWallet from './pages/VerifyRippleWallet/VerifyRippleWallet';
 import ImportAlgoWallet from './pages/ImportAlgoWallet/ImportAlgoWallet';
 import Backdrop from './components/UI/Backdrop/Backdrop'
-import Wallet from './pages/Wallet/Wallet';
-import Transactions from './pages/Transactions/Transactions';
-import AssetManager from './pages/AssetManager/AssetManager';
-import Settings from './pages/Settings/Settings';
-import NotFound from './pages/NotFound/NotFound';
-import ImportRippleWallet from './pages/ImportRippleWallet/ImportRippleWallet';
-import Exchange from './pages/Exchange/Exchange';
-import NewAssets from './pages/NewAssets/NewAssets';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import useFingerprint from './Hooks/Fingerprint';
 import PrivateRoute from './privateRoute'
 import ActiveWalletPassphrase from './Hooks/ActiveWalletPassphrase';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+const Wallet = React.lazy(() => import("./pages/Wallet/Wallet"));
+const Transactions = React.lazy(() => import('./pages/Transactions/Transactions'));
+const AssetManager = React.lazy(() => import('./pages/AssetManager/AssetManager'));
+const Settings = React.lazy(() => import('./pages/Settings/Settings'));
+const NotFound = React.lazy(() => import('./pages/NotFound/NotFound'));
+const Exchange = React.lazy(() => import('./pages/Exchange/Exchange'));
+const NewAssets = React.lazy(() => import('./pages/NewAssets/NewAssets'));
+
+
 
 function App() {
   
@@ -47,50 +50,52 @@ function App() {
                     classNames="page"
                     key={location.key}
                   >
-                    <Switch location={location}>
-                      <Route exact path="/">
-                        <Redirect to="/login" />
-                      </Route>
-                      <Route exact path={['/signup', '/login']} component={Auth} />
-                      
-                      <PrivateRoute exact path={["/wallet-setup", "/create-wallet"]}>
-                        <WalletSetup />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/create-wallet/:wallet">
-                        <CreateNewWallet />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/verify-wallet/algo">
-                        <VerifyAlgoWallet />
-                      </PrivateRoute>
-                      {/* <PrivateRoute exact path="/verify-wallet/xrp">
-                        <VerifyRippleWallet />
-                      </PrivateRoute> */}
-                      <PrivateRoute exact path="/import-wallet/algo">
-                        <ImportAlgoWallet />
-                      </PrivateRoute>
-                      {/* <PrivateRoute exact path="/import-wallet/xrp">
-                        <ImportRippleWallet />
-                      </PrivateRoute> */}
-                      <PrivateRoute exact path="/wallet">
-                        <Wallet />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/transactions">
-                        <Transactions />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/asset-manager">
-                        <AssetManager />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/new-assets">
-                        <NewAssets />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/exchange">
-                        <Exchange />
-                      </PrivateRoute>
-                      <PrivateRoute exact path="/settings">
-                        <Settings />
-                      </PrivateRoute>
-                      <Route component={NotFound} />
-                    </Switch>
+                    <Suspense fallback={<Backdrop isOpen />}>
+                      <Switch location={location}>
+                        <Route exact path="/">
+                          <Redirect to="/login" />
+                        </Route>
+                        <Route exact path={['/signup', '/login']} component={Auth} />
+                        
+                        <PrivateRoute exact path={["/wallet-setup", "/create-wallet"]}>
+                          <WalletSetup />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/create-wallet/:wallet">
+                          <CreateNewWallet />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/verify-wallet/algo">
+                          <VerifyAlgoWallet />
+                        </PrivateRoute>
+                        {/* <PrivateRoute exact path="/verify-wallet/xrp">
+                          <VerifyRippleWallet />
+                        </PrivateRoute> */}
+                        <PrivateRoute exact path="/import-wallet/algo">
+                          <ImportAlgoWallet />
+                        </PrivateRoute>
+                        {/* <PrivateRoute exact path="/import-wallet/xrp">
+                          <ImportRippleWallet />
+                        </PrivateRoute> */}
+                        <PrivateRoute exact path="/wallet">
+                          <Wallet />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/transactions">
+                          <Transactions />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/asset-manager">
+                          <AssetManager />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/new-assets">
+                          <NewAssets />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/exchange">
+                          <Exchange />
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/settings">
+                          <Settings />
+                        </PrivateRoute>
+                        <Route component={NotFound} />
+                      </Switch>
+                    </Suspense>
                   </CSSTransition>
                 </TransitionGroup>
               </>
--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -6,13 +6,13 @@ import { useSelector } from 'react-redux';
 import { CirclesWithBar } from  'react-loader-spinner'
 
 
-export default function SimpleBackdrop() {
+export default function SimpleBackdrop({ isOpen }) {
   const open = useSelector(state => state.backdrop.open)
 
   return (
     <Backdrop
       sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={open}
+      open={open || isOpen}
       // onClick={handleClose}
     >
       {/* <CircularProgress color="inherit" /> */}
